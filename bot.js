c45c24112b0947f6f8ccf157dfef11907238b14c
jfomby <fomby@Fomby-Alteryx.local>
2015-05-29 20:18:11 -0600

    added john as a username

--- bot.js
+++ bot.js
@@ -3,11 +3,11 @@ module.exports = function(req, res, next) {
   var botPayload = {};
   if (userName === "pradliff") {
     botPayload.text = "Paul, slackBot told me you're his favorite.";
-  } else if (userName === "poornima") {
+  } else if (userName === "john") {
     botPayload.text = "I'm back!!!! WARNING: Poornima is speaking. Everyone keep calm and it will all be over soon.";
   }
 
-  var names = ["pradliff", "poornima"];
+  var names = ["pradliff", "john"];
   if (names.indexOf(userName) >= 0) {
     return res.status(200).json(botPayload);
   } else {
